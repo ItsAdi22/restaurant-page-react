2bc99053ace238687dfa539c936c755db748febc
Aditya Mondkar <adityamondkar111@gmail.com>
2023-07-12 19:45:46 +0530

    added pricing page

---
+++ src/components/App1.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import Home from './components/Home';
+
+function App1() {
+  return (
+    <>
+    <Header></Header>
+    </>
+  );
+}
+
+export default App1;
